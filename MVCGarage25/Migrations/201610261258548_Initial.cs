8c71dfe15f55e917fa77457baf40e40f10af95ad
TimoKinnunen <timo.kinnunen@bredband.net>
2016-10-26 15:07:50 +0200

    Nytt försök

--- MVCGarage25/Migrations/201610261258548_Initial.cs
+++ MVCGarage25/Migrations/201610261258548_Initial.cs
@@ -3,7 +3,7 @@ namespace MVCGarage25.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Fnatteinit : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
